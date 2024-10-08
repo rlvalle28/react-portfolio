8c27ec3240d402a3b0ff8ecb1b49dc7e42e5e889
Ralph <ralphlaurencevalle28@gmail.com>
2024-10-08 14:03:52 +0800

    react-porfolio

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,10 +14,14 @@ const Footer = () => {
         <div className='footer'>
         <img src={logo_img}></img>
         <div className='react-icons'>
-        <FaFacebook />
-        <FaGithub />
-        <FaInstagramSquare />
-        <AiFillTikTok />
+        <a href="https://www.facebook.com/rencevalle.28" target="_blank" rel="noopener noreferrer">
+                        <FaFacebook />
+                    </a>
+                    <a href="https://github.com/rlvalle28" target="_blank" rel="noopener noreferrer">
+                        <FaGithub />
+                    </a>
+                    <FaInstagramSquare />
+                    <AiFillTikTok />
         </div>
         </div>
       </div>
